c9c7b8224e46bd4d0c56f2fa723b23aa62ba30fb
Denivaldo <110608377+Denivaldo-bit@users.noreply.github.com>
2023-04-20 17:41:39 -0300

    Add files via upload

---
+++ cachorro1/Cachorro1.java
@@ -0,0 +1,20 @@
+
+package cachorro1;
+
+public class Cachorro1 {
+    
+    int tamanho;
+    String raça;
+    
+
+    public static void main(String[] args) {
+        
+        System.out.println("au au au");
+        
+       
+        
+        
+
+    }
+    
+}
---
+++ cachorro1/cachorro1teste.java
@@ -0,0 +1,25 @@
+
+package cachorro1;
+
+public class cachorro1teste {
+    
+    public static void main(String[]args){
+    
+    Cachorro beatle = new cachorro ();
+    beatle.raça = "beatle";
+    beatle.tamanho = 40;
+    beatle.latir();
+    
+    
+    
+    
+        
+        
+    }
+        
+        
+    
+    
+}
+    
+}
